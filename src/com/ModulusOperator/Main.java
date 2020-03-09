ca9da3bbf501f31425999799ce42bceb1335c1ef
saleenadelgado <Saleena.delgado.2022@student.lhschools.org>
2020-03-09 12:30:35 -0400

    ternary Operator built for hours

--- src/com/ModulusOperator/Main.java
+++ src/com/ModulusOperator/Main.java
@@ -1,8 +1,25 @@
 package com.ModulusOperator;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	ModulusClock();
+    }
+
+    public static void ModulusClock()  {
+    int hours;
+    int minutes;
+    Scanner keyboard;
+    keyboard= new Scanner (System.in);
+  System.out.println("Enter a value for hours");
+hours = keyboard.nextInt();
+System.out.println("Enter a value for minutes");
+minutes = keyboard.nextInt();
+       hours = minutes >=60 ? ++hours : hours;
+     //minutes = minutes 60%
+     // minutes %= 60;
+System.out.println(hours + ": " + minutes);
     }
 }
